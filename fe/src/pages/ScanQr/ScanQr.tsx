ea3f1c73655dfa83de63d4c9b3f8c0be6791a676
pavan jadhav <pavanjadhav5331@gmail.com>
2025-10-29 15:25:29 +0530

    ScanQr

--- fe/src/pages/ScanQr/ScanQr.tsx
+++ fe/src/pages/ScanQr/ScanQr.tsx
@@ -89,7 +89,7 @@ const ScanQr: React.FC = () => {
           Scanned Data: <strong>{scannedData}</strong>
         </p>
       )}
-      <div className="mt-30">
+      <div className="mt-100">
       <Button
         text="Logout"
         varient="secondary"
